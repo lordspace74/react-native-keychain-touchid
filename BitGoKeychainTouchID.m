bf9041478e6443b69a4efea8831d97130fec5bec
Antti Lustila <antti.lustila@hiq.fi>
2017-01-12 09:40:58 +0200

    Use a more restricting retrieval condition when storing credentials.

--- BitGoKeychainTouchID.m
+++ BitGoKeychainTouchID.m
@@ -13,15 +13,15 @@ RCT_EXPORT_MODULE()
 }
 
 RCT_EXPORT_METHOD(storeCredentialsForAccount:(NSString *)email withToken:(NSString *)token){
-    
+
     CFErrorRef error = NULL;
     SecAccessControlRef sacObject = SecAccessControlCreateWithFlags(
-                                                                    kCFAllocatorDefault,
-                                                                    kSecAttrAccessibleWhenPasscodeSetThisDeviceOnly,
-                                                                    kSecAccessControlUserPresence,
-                                                                    &error
-                                                                    );
-    
+      kCFAllocatorDefault,
+      kSecAttrAccessibleWhenPasscodeSetThisDeviceOnly,
+      kSecAccessControlTouchIDCurrentSet, // Restrict keychain item access to TouchID, and invalidate access when TouchID fingerprint set is changed  
+      &error
+    );
+
     // store a token by saving it using the keychain with only touch ID accessibility
     NSDictionary *dict = @{
                            (__bridge id)(kSecClass): (__bridge id)kSecClassGenericPassword,
@@ -30,20 +30,20 @@ RCT_EXPORT_METHOD(storeCredentialsForAccount:(NSString *)email withToken:(NSStri
                            (__bridge id)kSecAttrAccount: email,
                            (__bridge id)kSecValueData: [token dataUsingEncoding:NSUTF8StringEncoding]
                            };
-    
+
     OSStatus err = SecItemAdd((__bridge CFDictionaryRef) dict, NULL);
     NSLog(@"Store Credentials For Account result: %@", [self keychainErrorToString:err]);
-    
+
     if(err == errSecSuccess || err == errSecDuplicateItem){
         NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
         [defaults setObject:@YES forKey:[self userDefaultsStorageIndicatorKey]];
         [defaults synchronize];
     }
-    
+
 }
 
 RCT_EXPORT_METHOD(hasCredentialsWithCallback:(RCTResponseSenderBlock)callback){
-    
+
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     NSNumber *isStored = [defaults objectForKey:[self userDefaultsStorageIndicatorKey]];
     if(isStored && isStored.boolValue == YES){
@@ -51,11 +51,11 @@ RCT_EXPORT_METHOD(hasCredentialsWithCallback:(RCTResponseSenderBlock)callback){
         return;
     }
     callback(@[[NSNull null], @NO]);
-    
+
 }
 
 RCT_EXPORT_METHOD(retrieveCredentialsWithTouchIDPrompt:(NSString *)promptString andCallback:(RCTResponseSenderBlock)callback){
-    
+
     NSDictionary *dict = @{
                            (__bridge id)(kSecClass): (__bridge id)kSecClassGenericPassword,
                            (__bridge id)kSecAttrService: [self serviceName],
@@ -63,13 +63,13 @@ RCT_EXPORT_METHOD(retrieveCredentialsWithTouchIDPrompt:(NSString *)promptString 
                            (__bridge id)kSecUseOperationPrompt: promptString,
                            (__bridge id)kSecReturnData: (__bridge id)kCFBooleanTrue,
                            };
-    
-    
+
+
     NSDictionary* found = nil;
     CFTypeRef foundCF = NULL;
     OSStatus err = SecItemCopyMatching((__bridge CFDictionaryRef) dict, (CFTypeRef*)&foundCF);
     NSLog(@"Retrieve Credentials For Account result: %@", [self keychainErrorToString:err]);
-    
+
     found = (__bridge NSDictionary*)(foundCF);
     if (!found){
         NSError *error = [NSError errorWithDomain:[self serviceName] code:err userInfo:nil];
@@ -77,60 +77,60 @@ RCT_EXPORT_METHOD(retrieveCredentialsWithTouchIDPrompt:(NSString *)promptString 
         callback(@[jsError]);
         return;
     }
-    
+
     // Found
     NSString* username = (NSString*) [found objectForKey:(__bridge id)(kSecAttrAccount)];
     NSString* token = [[NSString alloc] initWithData:[found objectForKey:(__bridge id)(kSecValueData)] encoding:NSUTF8StringEncoding];
-    
+
     NSDictionary *response = @{
                                @"username": username,
                                @"token": token
                                };
     callback(@[[NSNull null], response]);
-    
+
 }
 
 RCT_EXPORT_METHOD(deleteCredentials){
-    
+
     NSDictionary *dict = @{
                            (__bridge id)(kSecClass): (__bridge id)kSecClassGenericPassword,
                            (__bridge id)kSecAttrService: [self serviceName]
                            };
     OSStatus status = SecItemDelete((__bridge CFDictionaryRef)dict);
     NSLog(@"Delete Credentials For Account result: %@", [self keychainErrorToString:status]);
-    
+
     if(status == errSecSuccess){
         NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
         [defaults setObject:@NO forKey:[self userDefaultsStorageIndicatorKey]];
         [defaults synchronize];
     }
-    
+
 }
 
 - (NSString *)keychainErrorToString:(OSStatus)error {
     NSString *message = [NSString stringWithFormat:@"%ld", (long)error];
-    
+
     switch (error) {
         case errSecSuccess:
             message = @"success";
             break;
-            
+
         case errSecDuplicateItem:
             message = @"error item already exists";
             break;
-            
+
         case errSecItemNotFound :
             message = @"error item not found";
             break;
-            
+
         case errSecAuthFailed:
             message = @"error item authentication failed";
             break;
-            
+
         default:
             break;
     }
-    
+
     return message;
 }
 
